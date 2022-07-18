7fcfb730358c6a430496e7fa9222d10f22eb0fa2
Joshua Jackson <jjackson@kallisteconsulting.com>
2022-07-17 23:29:06 -0500

    Config

---
+++ Config.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Krag;
+
+class Config implements \IteratorAggregate
+{
+
+    public function __construct(
+        public array $settings = [],
+    ) {
+        if (file_exists('config.php'))
+        {
+            include('config.php');
+            $fileSettings = get_defined_vars();
+            unset($fileSettings['settings']);
+            $this->settings = array_merge($fileSettings, $settings);
+        }
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->settings))
+        {
+            return $this->settings[$name];
+        }
+        return null;
+    }
+
+    public function getIterator() : \Traversable
+    {
+        return new \ArrayIterator($this->settings);
+    }
+
+}
+
+?>
