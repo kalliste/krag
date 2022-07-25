0af7f6d3f92aa994f0c1fb05dfb18929b210b3bd
Joshua Jackson <jjackson@kallisteconsulting.com>
2022-07-25 15:43:50 -0500

    Better plan for PHP templates

--- src/Views.php
+++ src/Views.php
@@ -6,8 +6,6 @@ use Psr\Http\Message\ResponseInterface;
 
 class Views implements ViewsInterface
 {
-    private ResponseInterface $response;
-
     public function __construct(private string $templatePath = 'templates')
     {
     }
@@ -19,17 +17,26 @@ class Views implements ViewsInterface
     }
 
     /**
+     * @param array<string, mixed> $data
+     */
+    protected function fillTemplate(string $fileName, array $data): void
+    {
+        extract($data);
+        require(func_get_arg(0));
+    }
+
+    /**
      * @param array<string, mixed> $methodData
      * @param array<string, mixed> $globalData
      */
     public function render(string $controllerName, string $methodName, array $methodData, array $globalData, RoutingInterface $routing, ResponseInterface $response): ResponseInterface
     {
-        $this->response = $response;
-        extract($globalData);
-        extract($methodData);
-        ob_start();
-        include($this->templateFile($controllerName, $methodName));
-        $this->response->getBody()->write(ob_get_clean());
+        $fileName = $this->templateFile($controllerName, $methodName);
+        if (file_exists($fileName)) {
+            ob_start();
+            $this->fillTemplate($fileName, array_merge(compact('routing'), $globalData, $methodData));
+            $response->getBody()->write(ob_get_clean());
+        }
         return $response;
     }
 }
